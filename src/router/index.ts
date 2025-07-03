4303d93bfa6150cfa361892da73b9ef24709d397
IanMV <ianmatheusv54@gmail.com>
2025-07-03 20:35:11 -0300

    FEAT: Adicionando página inicial,adicionando main.css e configurando roteador e App.vue para desenvolvimento da barra de navegação

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MapaCrime from '@/views/MapaCrime.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      redirect: '/mapa-de-crime',
+    },
+    {
+      path: '/mapa-de-crime',
+      component: MapaCrime,
+    },
   ],
 })
 
