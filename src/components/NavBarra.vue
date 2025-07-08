2ae4843fe6498aa06fc4811a72a7e505e13b8020
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2025-07-08 09:31:27 -0300

    Feat: Inserindo Animações na Barra

--- src/components/NavBarra.vue
+++ src/components/NavBarra.vue
@@ -1,19 +1,45 @@
 <script setup lang="ts">
-
+import {ref} from 'vue'
+ const isExpanse = ref<boolean>(false)
+ function toggleBar() {
+  isExpanse.value = !isExpanse.value
+ }
 </script>
 
 <template>
- <nav class="container-nav">
-    <span class="fa-solid fa-bars"></span>
+ <nav :class="['container-nav', {isExpanse}]">
+  <div class="icons">
+    <span class="fa-solid fa-bars" @click="toggleBar"></span>
     <span class="fa-solid fa-user"></span>
     <span class="fa-solid fa-gear"></span>
-    <h2>reclame</h2>
+  </div>
  </nav>
 </template>
 
 <style scoped>
 .container-nav{
-  background-color: black;
+   background-color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;       
+  height: 100vh;
+  width: 4.6vw;
+  .icons{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 6rem;
+    margin-top: 2vw;
+  }
+  span{
+    font-size: 2rem;
+    
+  }
+}
+.container-nav.isExpanse{
+  width:  20vw;
+  align-items: left;
+  transition: .5s ease-in-out;
 }
 h1 {
   font-weight: 500;
