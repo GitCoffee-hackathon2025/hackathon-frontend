700940a7c3ca8498332584afb955676d68643405
IanMV <ianmatheusv54@gmail.com>
2025-07-15 09:30:59 -0300

    FEAT: Agora a nav abre quando passa o mouse, mudado os ícones e arrumado tamanhos no css

--- src/components/NavBarra.vue
+++ src/components/NavBarra.vue
@@ -1,31 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const isExpanse = ref<boolean>(false)
-function toggleBar() {
+function openBar() {
   isExpanse.value = !isExpanse.value
 }
 </script>
 
 <template>
-  <nav :class="['container-nav', { isExpanse }]">
+  <nav @mouseover="openBar" @mouseout="openBar" :class="['container-nav', { isExpanse }]">
     <ul class="icons">
       <li>
-        <span class="fa-solid fa-bars" @click="toggleBar"></span>
-        <p :class="{ visible: isExpanse }">Bars</p>
+        <img src="../assets/iconeconfiguracao.svg" alt="" />
+        <p :class="{ visible: isExpanse }">Configurações</p>
       </li>
       <li>
-        <span class="fa-solid fa-user"></span>
-        <p :class="{ visible: isExpanse }">User</p>
+        <img src="../assets/iconeusuario.svg" alt="" />
+        <p :class="{ visible: isExpanse }">Usuário</p>
       </li>
       <li>
-        <span class="fa-solid fa-gear"></span>
-        <p :class="{ visible: isExpanse }">Settings</p>
+        <img src="../assets/iconemapa.svg" alt="" />
+        <p :class="{ visible: isExpanse }">Mapa de denúncias</p>
+      </li>
+      <li class="icone-informacao">
+        <img src="../assets/iconeinformacao.svg" alt="" />
+        <p :class="{ visible: isExpanse }">Sobre o site</p>
       </li>
     </ul>
   </nav>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .container-nav {
   background-color: black;
   display: flex;
@@ -33,48 +37,56 @@ function toggleBar() {
   align-items: center;
   height: 100vh;
   width: 4.6rem;
-  transition: width 0.3s ease-in-out;
+  transition: width 0.3s ease;
   overflow: hidden;
-}
 
-.container-nav.isExpanse {
-  width: 16rem;
-}
+  &.isExpanse {
+    width: 16rem;
+  }
 
-.icons {
+  .icons {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   padding-left: 1.2rem;
   width: 100%;
+  height: 100%;
   gap: 3rem;
-  margin-top: 2rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    color: white;
+    position: relative;
+    width: 100%;
+
+    img {
+      width: 2rem;
+      color: white;
+      cursor: pointer;
+    }
+
+    p {
+      color: white;
+      opacity: 0;
+      transform: translateX(1vw);
+      transition: opacity 0.3s ease, transform 0.3s ease;
+      white-space: nowrap;
+
+      &.visible {
+        opacity: 1;
+        transform: translateX(1vw);
+      }
+    }
+
+    &.icone-informacao {
+      margin-top: auto;
+    }
+  }
 }
 
-.icons li {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  color: white;
-  position: relative;
-}
-
-.icons p {
-  color: white;
-  opacity: 0;
-  transform: translateX(-20px);
-  transition: opacity 0.3s ease, transform 0.3s ease;
-  white-space: nowrap;
-}
-
-.icons p.visible {
-  opacity: 1;
-  transform: translateX(2rem);
-}
-  
-span {
-  font-size: 2rem;
-  color: white;
-  cursor: pointer;
 }
 </style>
