8caffd2f79dd71cfa0fe815253426e337898ef90
IanMV <ianmatheusv54@gmail.com>
2025-08-23 16:11:38 -0300

    REFACT: Barra de navegação no componente principal

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import NavBarra from '@/components/NavBarra.vue'
 </script>
 
 <template>
-    <router-view></router-view>
+  <NavBarra />
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
--- src/views/MapadeDenuncias/views/PaginaInicial.vue
+++ src/views/MapadeDenuncias/views/PaginaInicial.vue
@@ -4,7 +4,6 @@ import { onMounted, nextTick } from 'vue'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import DadosBairro from '@/views/MapadeDenuncias/components/DadosBairros.vue'
-import NavBarra from '@/components/NavBarra.vue'
 import BarraPesquisa from '@/views/MapadeDenuncias/components/BarraPesquisa.vue'
 
 let map: L.Map | null = null
@@ -88,7 +87,6 @@ onMounted(() => {
 </script>
 
 <template>
-  <NavBarra />
   <main>
     <BarraPesquisa />
     <div class="map-container">
