b63caa41b3cf9f3a422e45beaeb566cba480fec9
Erick-Henrique-Pedrini <erickpedrinebr@gmai.com>
2025-08-28 11:18:34 -0300

    FEAT:fetch Req

--- src/store/Bairro.ts
+++ src/store/Bairro.ts
@@ -4,7 +4,7 @@ import type L from 'leaflet'
 
 export const useBairroStore = defineStore('bairro', () => {
   const selectedData = ref<Record<string, any> | null>(null)
-  const mapInstance = ref<L.Map | null>(null) 
+  const mapInstance = ref<L.Map | null>(null)
 
   function setMap(map: L.Map) {
     mapInstance.value = map
@@ -17,6 +17,22 @@ export const useBairroStore = defineStore('bairro', () => {
   function clearBairro() {
     selectedData.value = null
   }
+  
+  const getDataBairro = async (idData: number): Promise<any> => {
+    console.log("ola")
+    const response = await fetch(`http://localhost:3000/reportsByNeighborhood/${idData}`, { method: 'GET' })
 
-  return { selectedData, mapInstance, setMap, selectBairro, clearBairro }
+    if (!response.ok) {
+      const errText = await response.text()
+      console.error('Erro HTTP:', response.status, errText)
+      throw new Error(`Erro na requisição: ${response.status}`)
+    }
+
+
+    const data = await response.json()
+    console.log('Resposta JSON:', data)
+    return data
+  }
+
+  return { setMap, selectedData, selectBairro, clearBairro, getDataBairro }
 })
